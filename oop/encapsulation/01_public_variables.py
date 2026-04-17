8112f1df912cc977effb3b1f5d0bc42978d09010
Mustafa Quraishi <mustafaqr7@gmail.com>
2026-04-16 19:21:52 -0700

    Public variables file added

---
+++ oop/encapsulation/01_public_variables.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+
+student1 = Student("Alice", 20)
+print(student1.name)  # Output: Alice
+print(student1.age)   # Output: 20
+
+
